3cfff5eea4791a84b18c061657035e5dd3dc6f8c
ajithbharathi <ajithbharathi11@gmail.com>
2025-06-15 06:07:48 +0530

    git changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
+import { Observable, of } from 'rxjs';
 import { AppService } from './app.service';
 
 @Component({
@@ -7,20 +7,78 @@ import { AppService } from './app.service';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ng-test-app';
-  subName:string = '';
+  subName: string = '';
+
+  of1: Observable<string> = of('of 1');
+  of2!: string;
+  of3?: Observable<string>;
+  of3Value!: string;
+
+
+  obs1: Observable<number>;
+  obs2: Observable<number>;
+  obs2Value: number = 0;
+
   constructor(private appService: AppService) {
     this.appService.mysubData.subscribe((data) => {
       this.subName = data;
-    }
-    );
-   
+    });
+
+    // *** using of method ***
+    this.of3 = of('of 3');
+
+    // *** manaual observable ***
+    this.obs1 = new Observable<number>((observer) => {
+      observer.next(Math.floor(Math.random() * 10));
+    })
+
+    this.obs2 = new Observable<number>((observer) => {
+      observer.next(Math.floor(Math.random() * 100))
+      
+    })
+  }
+
+  ngOnInit() {
+    of('of 2').subscribe((data) => {
+      this.of2 = data;
+    });
+
+    this.obs2.subscribe((data) => {
+      this.obs2Value = data;
+    });
+  }
+
+
+  changeof1() {
+    this.of1 = of('of 1 - ' + Math.floor(Math.random() * 10));
+  }
+
+   getof3() {
+    this.of3?.subscribe((data) => {
+      this.of3Value = data;
+    })
+  }
+
+   onChangeObs2() {
+   this.obs2 = new Observable<number>((observer) => {
+      observer.next(Math.floor(Math.random() * 100))
+    })
+
+    this.obs2.subscribe((data) => {
+      this.obs2Value = data;
+    });
+
   }
 
   onNameChange(e: any) {
     const value = e.target.value;
     this.appService.parentname.emit(value);
-    
+
   }
+
+ 
+
+ 
 }
